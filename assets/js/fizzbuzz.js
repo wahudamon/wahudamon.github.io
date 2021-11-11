dedf2579babf3067bf259a2a2a2788d6371a8d5e
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2021-11-11 11:38:47 +0700

    feat: show fizz buzz results

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -1,15 +1,27 @@
-function fizzBuzz(totalNumber) {
+let totalNumber = 0;
+
+let resultsParent = document.getElementById('result-parent')
+
+function getTotalNumber() {
+  totalNumber = document.getElementById('input-total').value
+
   for(let index = 1; index <= totalNumber; index++) {
-    if(index % 3 == 0 && index % 5 == 0) {
-      console.log('fizz buzz')
-    } else if(index % 3 == 0) {
-      console.log('fizz')
-    } else if(index % 5 == 0) {
-      console.log('buzz')
-    } else {
-      console.log(index)
-    }
+    fizzbuzz(index)
   }
 }
 
-fizzBuzz(20)
+function fizzbuzz(index) {
+  let resultElement = document.createElement('li')
+
+  if(index % 3 == 0 && index % 5 == 0) {
+    resultElement.innerText = 'fizz buzz'
+  } else if(index % 3 == 0) {
+    resultElement.innerText = 'fizz'
+  } else if(index % 5 == 0) {
+    resultElement.innerText = 'buzz'
+  } else {
+    resultElement.innerText = index
+  }
+
+  resultsParent.append(resultElement)
+}
