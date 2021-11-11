af86be10bb7793729088908b77709acc346ee62d
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2021-11-11 12:16:08 +0700

    feat: create a function to remove the older childs inside result parent element

--- assets/js/fibonacci.js
+++ assets/js/fibonacci.js
@@ -1,13 +1,27 @@
-let totalNumber = 0, firstNumber = 0, secondNumber = 1, nextNumber
+let totalNumber = 0, firstNumber = 0, secondNumber = 1, nextNumber = 0
 
 let resultsParent = document.getElementById('result-parent')
 
 function resetInput() {
+  firstNumber = 0
+  secondNumber = 1
+  totalNumber = 0
   document.getElementById('input-total').value = null
+}
+
+function removeOlderChilds() {
+  if(resultsParent.hasChildNodes()) {
+    while(resultsParent.firstChild)
+      resultsParent.removeChild(resultsParent.firstChild)
+  } else {
+    console.log("result-parent element has no child.")
+  }
 }
 
 function getTotalNumber() {
   totalNumber = document.getElementById('input-total').value
+
+  removeOlderChilds()
 
   for(let index = 1; index <= totalNumber; index++) {
     fibonacci()
--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -1,13 +1,25 @@
-let totalNumber = 0;
+let totalNumber = 0
 
 let resultsParent = document.getElementById('result-parent')
 
 function resetInput() {
+  totalNumber = 0
   document.getElementById('input-total').value = null
+}
+
+function removeOlderChilds() {
+  if(resultsParent.hasChildNodes()) {
+    while(resultsParent.firstChild)
+      resultsParent.removeChild(resultsParent.firstChild)
+  } else {
+    console.log("result-parent element has no child.")
+  }
 }
 
 function getTotalNumber() {
   totalNumber = document.getElementById('input-total').value
+
+  removeOlderChilds()
 
   for(let index = 1; index <= totalNumber; index++) {
     fizzbuzz(index)
